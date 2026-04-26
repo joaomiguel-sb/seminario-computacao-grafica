a591889f2c5207210d9d735d03323f698dbd4931
Davi Cavalcante <93160711+DaviMacielCavalcante@users.noreply.github.com>
2026-04-26 17:16:28 -0300

    🚧 wip: cração do crouch, mas ainda não tem todos os ajustes feitos

--- metroidvania/scripts/player.gd
+++ metroidvania/scripts/player.gd
@@ -11,13 +11,28 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		
+	# Handle crouch
+	if Input.is_action_pressed("crouch") and is_on_floor():
+		$Sprite2D.scale.y = 0.5
+		$Sprite2D.position.y = 16
+		$CollisionShape2D.shape.height = 20
+		
+	if not Input.is_action_pressed("crouch"):
+		$Sprite2D.scale.y = 1
+		$Sprite2D.position.y = 0
+		$CollisionShape2D.shape.height = 40
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("left", "right")
 	if direction:
+		if direction == -1:
+			$Sprite2D.flip_h = true 
+		if direction == 1:
+			$Sprite2D.flip_h = false
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
